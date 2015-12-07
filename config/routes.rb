b899b0463d075b0c92d311cef6f52cb2967371af
samar_singh <bills23@gmail.com>
2015-12-07 07:37:33 +0000

    routes partial in the routes file and some changes in the recipe_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 root 'pages#home'
 get '/home' , to: 'pages#home'
+get 'recipes', to: 'recipes#index'
+get 'recipes/new', to: 'recipes#new', as: 'new_recipes'
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -24,6 +24,7 @@ class RecipeTest < ActiveSupport::TestCase
    @recipe.name = "a" * 101
    assert_not @recipe.valid?
  end
+ 
  test "name length should not be too short" do
    @recipe.name = "aaaa"
    assert_not @recipe.valid?
@@ -33,10 +34,12 @@ class RecipeTest < ActiveSupport::TestCase
    @recipe.summary = " "
    assert_not @recipe.valid?
  end
+ 
  test "summary length should not be too long" do
    @recipe.summary = "a" * 151
    assert_not @recipe.valid?
  end
+ 
  test "summary length should not be too short" do
    @recipe.summary = "a" * 9
    assert_not @recipe.valid?
@@ -46,10 +49,12 @@ class RecipeTest < ActiveSupport::TestCase
    @recipe.description = " "
    assert_not @recipe.valid?
  end
+ 
  test "description length should not be too long" do
    @recipe.description = "a" * 501
    assert_not @recipe.valid?
  end
+ 
  test "description length should not be too short" do
    @recipe.description = "a" * 19
    assert_not @recipe.valid?
