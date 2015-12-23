2f0225a61241726508e30477ef6686224184fe13
samar_singh <bills23@gmail.com>
2015-12-23 06:38:45 +0000

    Create new and edit forms and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
 root 'pages#home'
-get '/home' , to: 'pages#home'
-get 'recipes', to: 'recipes#index'
-get 'recipes/new', to: 'recipes#new', as: 'new_recipes'
+  resources :recipes
 end
